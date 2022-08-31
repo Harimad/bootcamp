718fbac74fa744134563d05c2328e7f7f78f1409
Harimad <leesh68481@gmail.com>
2022-09-01 01:26:23 +0900

    4_reuse Child Component prepost to main.js

--- vue_Practice/project/src/views/4_reuse/PageView.vue
+++ vue_Practice/project/src/views/4_reuse/PageView.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
     <page-title title="고객목록" />
-    <PageTitle />
-    <title />
   </div>
 </template>
 <script>
-import PageTitle from '@/components/fragments/PageTitle.vue'
+// import PageTitle from '@/components/fragments/PageTitle.vue'
 export default {
-  components: { PageTitle },
+  // components: { PageTitle },
   data() {
     return {
       sampleData: ''
